0dd8f57dc18712546459b36f077b5c6a46718c25
tizarp <61999323+tizarp@users.noreply.github.com>
2020-05-14 21:57:06 +0700

    Add files via upload

---
+++ IPolymorphism_Interface (1)/IPolymorphism_Interface/IPolymorphism_Interface/IPolymorphism_Interface/printerwindows.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism._1
+{
+    public interface printerwindows
+    {
+        void show();
+        void print();
+    }
+
+    class Epson : printerwindows
+
+    {
+        public void show()
+        {
+            Console.WriteLine("Epson display dimension : 10*11");
+        }
+        public void print()
+        {
+            Console.WriteLine("Epson printer printing.....");
+        }
+    }
+
+    class Canon : printerwindows
+    {
+        public void show()
+        {
+            Console.WriteLine("Canon display dimension : 9.5*12");
+        }
+        public void print()
+        {
+            Console.WriteLine("Canon printer printing.....");
+        }
+    }
+
+    class Laserjet : printerwindows
+    {
+        public void show()
+        {
+            Console.WriteLine("Laserjet display dimension : 12*12");
+        }
+        public void print()
+        {
+            Console.WriteLine("Laserjet printer printing.....");
+        }
+    }
+}
---
+++ IPolymorphism_Interface (1)/IPolymorphism_Interface/IPolymorphism_Interface/IPolymorphism_Interface/program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Polymorphism._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printerwindows printer;
+
+            Console.WriteLine("Pilih Printer:");
+            Console.WriteLine("1.Epson");
+            Console.WriteLine("2.Canon");
+            Console.WriteLine("3.Laserjet");
+
+            Console.WriteLine("Nomor printer [1,2,3] : ");
+            int nomorprinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorprinter == 1)
+            {
+                printer = new Epson();
+            }
+            else if (nomorprinter == 2)
+            {
+                printer = new Canon();
+            }
+            else
+            {
+                printer = new Laserjet();
+            }
+
+            printer.show();
+            printer.print();
+
+            Console.ReadKey();
+        }
+    }
+}
