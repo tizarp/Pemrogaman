f5bf4fb18920b78a513fec0885b857cb6b503a12
tizarp <61999323+tizarp@users.noreply.github.com>
2020-07-30 22:00:47 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/class anak/kurang.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.FP
+{
+    public class kurang : barang
+    {
+
+        public override string stok()
+        {
+            return "harus ditambah";
+        }
+    }
+}
